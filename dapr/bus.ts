e9767f566ae50799a2ed0b0ea9d5b47f97f00ccc
Phil Kedy <phil.kedy@gmail.com>
2023-01-09 14:51:50 -0500

    Example tweaks

---
+++ dapr/bus.ts
@@ -0,0 +1,130 @@
+#!/usr/bin/env -S deno run
+import {
+  Application,
+  dapr,
+  DaprServerV1,
+  HTTPResponse,
+  HttpServerV1,
+  log,
+  RouterV1,
+  step,
+  unauthenticated,
+} from "https://deno.land/x/nanobus_config@v0.0.10/mod.ts";
+
+const app = new Application("dapr-example", "0.0.1");
+
+const people = app.interface("People", {
+  get: [
+    step(
+      "Get state",
+      dapr.GetState({
+        resource: "dapr",
+        store: "statestore",
+        key: "input.id",
+        notFoundError: "person_not_found",
+      }),
+    ),
+  ],
+
+  create: [
+    step(
+      "Save state",
+      dapr.SetState({
+        resource: "dapr",
+        store: "statestore",
+        items: [{
+          key: "input.id",
+          value: "input",
+        }],
+      }),
+    ),
+    step(
+      "Publish greeting message",
+      dapr.Publish({
+        resource: "dapr",
+        pubsub: "pubsub",
+        topic: "welcome",
+        data: `
+{
+  "type": "welcome.v1",
+  "data": "Welcome, " + input.firstName + " " + input.lastName + "!",
+}`,
+        codec: "cloudevents+json",
+      }),
+    ),
+    step(
+      "201 Created",
+      HTTPResponse({
+        status: 201,
+        headers: [
+          {
+            name: "Location",
+            value: '"{{host}}/people/" + input.id',
+          },
+        ],
+      }),
+    ),
+  ],
+});
+
+const events = app.interface("Events", {
+  onWelcome: [
+    step(
+      "Log the welcome message",
+      log("Event received: [%s] %q", "input.type", "input.data"),
+    ),
+  ],
+});
+
+app.authorizations({
+  [people.get]: unauthenticated,
+  [people.create]: unauthenticated,
+});
+
+app.transport(
+  "http",
+  HttpServerV1({
+    address: ":8080",
+    routers: [
+      RouterV1({
+        routes: [
+          {
+            method: "POST",
+            uri: "/people",
+            handler: people.create,
+          },
+          {
+            method: "GET",
+            uri: "/people/{id}",
+            handler: people.get,
+          },
+        ],
+      }),
+    ],
+  }),
+);
+
+app.transport(
+  "dapr",
+  DaprServerV1({
+    address: ":19090",
+    subscriptions: [
+      {
+        pubsub: "pubsub",
+        topic: "welcome",
+        codec: "cloudevents+json",
+        handler: events.onWelcome,
+        metadata: {},
+      },
+    ],
+  }),
+);
+
+app.error("person_not_found", {
+  type: "PersonNotFound",
+  code: "not_found",
+  title: "Person not found",
+  message: "Person for id {{ .key }} is not found",
+});
+
+app.emit();
